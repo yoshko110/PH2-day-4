91a1a270ca43380d06c9eea5042e3f94562276a0
“Yousef” <king_of_sword3@hotmail.com>
2023-12-07 20:00:07 +0300

    start

---
+++ index.js
@@ -0,0 +1,29 @@
+//You are given an array of numbers. Your task is to use iteration methods to filter and transform this array.
+
+const numbers = [10, 13, 20, 25, 38, 35, 40];
+
+/*****************************************************************
+Part 1: Filtering
+
+Write code to perform the following tasks using the filter method:
+Q1) Create a new array containing numbers that are greater than or equal to 25.
+Q2) Create a new array containing numbers that are divisible by 5.
+******************************************************************/
+
+/*****************************************************************
+Part 2: Mapping
+
+Write code to perform the following tasks using the map method:
+
+Q3) Create a new array that contains each number squared.
+Q4) Create a new array that contains each number multiplied by 2.
+******************************************************************/
+
+/*****************************************************************
+Part 3: Combining Filtering and Mapping
+
+Combine the filter and map methods to perform the following tasks:
+
+Q5) Filter the numbers that are greater than or equal to 20 and then square each of them.
+Q6) Filter the numbers that are divisible by 5 and then multiply each of them by 3.
+******************************************************************/
